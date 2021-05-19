0703d300552dc4d71f4508738a3343a6da94cb59
zqh0253 <3170104343@zju.edu.cn>
2021-05-19 17:56:29 +0800

    dynamics loss map enabled

--- PATrain.py
+++ PATrain.py
@@ -3,7 +3,7 @@ import time
 import argparse
 import torch
 import numpy as np
-import math
+import cv2
 
 from PIL import Image
 from torch.utils.data import DataLoader
@@ -22,38 +22,64 @@ class Canvas:
         self.R = R
         self.D = D
         self.U = U
-        self.canvas = np.zeros([int(R - L)+1, int(U - D)+1])
+        self.duration = 80
+        self.canvas = np.zeros([int(R - L) + 1, int(U - D) + 1])
+        self.dynamic_canvas = np.zeros([int(R - L) + 1, int(U - D) + 1, self.duration])
 
-    def _draw_dot(self, x, y, value):
+    def stash(self):
+        self.tmp_canvas = self.canvas.copy()
+        self.tmp_dynamic_canvas = self.dynamic_canvas.copy()
+        self.canvas = np.zeros([int(self.R - self.L) + 1, int(self.U - self.D) + 1])
+        self.dynamic_canvas = np.zeros([int(self.R - self.L) + 1, int(self.U - self.D) + 1, self.duration])
+
+    def flush(self):
+        self.canvas += self.tmp_canvas
+        self.dynamic_canvas += self.tmp_dynamic_canvas
+
+    def _draw_dot(self, x, y, value, idx, dynamic):
         cx = int(x - self.L)
         cy = int(y - self.D)
-        self.canvas[cx, cy] += value
+        if not dynamic:
+            self.canvas[cx, cy] = max(value, self.canvas[cx, cy])
+        else:
+            self.dynamic_canvas[cx, cy, idx] = max(value, self.dynamic_canvas[cx, cy, idx])
 
-    def _draw_line(self, x0, y0, x1, y1, value_f, k=2):
-        length = ((x0-x1)**2+(y0-y1)**2)**0.5
+    def _draw_line(self, x0, y0, x1, y1, value_f, idx, dynamic, k=2):
+        length = ((x0 - x1) ** 2 + (y0 - y1) ** 2) ** 0.5
         interval_num = max(int(length * k), 1)
         dx = (x1 - x0) / interval_num
         dy = (y1 - y0) / interval_num
         for i in range(interval_num):
             _x = x0 + dx * i
             _y = y0 + dy * i
-            self._draw_dot(_x, _y, value_f(_x, _y))
+            self._draw_dot(_x, _y, value_f(_x, _y), idx, dynamic)
 
-    def draw(self, xist0, yist0, xist1, yist1, value_f):
+    def draw(self, xist0, yist0, xist1, yist1, value_f, dynamic=False):
         # x: list of x-coordinates
         # y: list of y-coordinates
         # value_f: lambda function
-        for x0, y0, x1, y1 in zip(xist0, yist0, xist1, yist1):
-            self._draw_line(x0, y0, x1, y1, value_f)
+        for i, (x0, y0, x1, y1) in enumerate(zip(xist0, yist0, xist1, yist1)):
+            self._draw_line(x0, y0, x1, y1, value_f, i, dynamic)
 
     def to_image(self, name):
         img = Image.fromarray(self.canvas * 256).convert('L')
-        img.save(f'./canvas/{name}.png')
+        img.save(f'./canvas/background-{name}.png')
+
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        fps = 30
+        width = self.canvas.shape[1]
+        height = self.canvas.shape[0]
+        video = cv2.VideoWriter(f'./canvas/{name}.mp4', fourcc, float(fps), (width, height), False)
+        for i in range(self.duration):
+            content = (self.canvas + self.dynamic_canvas[..., i])*255
+            video.write(content.astype(np.uint8))
+        video.release()
+
 
 def loss_function(data, idx, coord, score, new_data, ora_coord, ora_score, ora_new_data):
     lane = data['lane_vector']  # batch_size, max_lane_num, 9, 6
-    lane_flat_x = lane[..., (0,2)].view(lane.shape[0], -1)
-    lane_flat_y = lane[..., (1,3)].view(lane.shape[0], -1)
+    lane_flat_x = lane[..., (0, 2)].view(lane.shape[0], -1)
+    lane_flat_y = lane[..., (1, 3)].view(lane.shape[0], -1)
     L, R = lane_flat_x.min(-1).values, lane_flat_x.max(-1).values
     D, U = lane_flat_y.min(-1).values, lane_flat_y.max(-1).values
     batch_size = coord.shape[0]
@@ -74,16 +100,19 @@ def loss_function(data, idx, coord, score, new_data, ora_coord, ora_score, ora_n
         cur_lane = lane[i]
         cur_lane = cur_lane[cur_lane[..., 4] == 15]
         cur_lane = cur_lane[cur_lane[..., 5] == 1][..., :4].T.tolist()
-        canvas.draw(cur_lane[0], cur_lane[1], cur_lane[2], cur_lane[3], lambda x, y: 0.2)
-
+        canvas.stash()
+        canvas.draw(cur_lane[0], cur_lane[1], cur_lane[2], cur_lane[3], lambda x, y: 0.6)
+        canvas.flush()
         # car iteration
+        canvas.stash()
         for j in range(ora_coord.shape[1]):
             # proposal iteration
             for k in range(ora_coord.shape[2]):
                 # build cost map with ora-COORD
                 ora_pred = ora_coord[i, j, k]
                 ora_pred = ora_pred.T.tolist()
-                canvas.draw(ora_pred[0][:-1], ora_pred[1][:-1], ora_pred[0][1:], ora_pred[1][1:], lambda x, y: 0.5)
+                canvas.draw(ora_pred[0][:-1], ora_pred[1][:-1], ora_pred[0][1:], ora_pred[1][1:], lambda x, y: 1.0, True)
+        canvas.flush()
         canvas.to_image(i)
 
         # calculate pro-active proposal loss
