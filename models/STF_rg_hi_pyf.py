654d4c07838f4725cd30d3da530ca9b6b75a4847
fenglan <fenglan18@outlook.com>
2021-05-20 23:15:25 +0800

    add new model

--- models/STF_rg_hi_pyf.py
+++ models/STF_rg_hi_pyf.py
@@ -11,9 +11,9 @@ from models.STF import STF
 import copy
 
 
-class STF_rg_hi(STF):
+class STF_rg_hi_pyf(STF):
     def __init__(self, cfg):
-        super(STF_rg_hi, self).__init__(cfg)
+        super(STF_rg_hi_pyf, self).__init__(cfg)
 
         # num of proposal
         prop_num = cfg['prop_num']
@@ -22,7 +22,7 @@ class STF_rg_hi(STF):
         dropout = cfg['dropout']
         N = cfg['model_layers_num']
         dec_out_size = cfg['out_dims']
-
+        pos_dim = 64
         c = copy.deepcopy
         attn = MultiHeadAttention(h, d_model, dropout)
         ff = PointerwiseFeedforward(d_model, d_model * 2, dropout)
@@ -45,6 +45,21 @@ class STF_rg_hi(STF):
             nn.ReLU(),
             nn.Linear(d_model, d_model, bias=True))
         self.social_enc = Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N)
+        self.pos_emb = nn.Sequential(
+            nn.Linear(3, pos_dim, bias=True),
+            nn.LayerNorm(pos_dim),
+            nn.ReLU(),
+            nn.Linear(pos_dim, pos_dim, bias=True))
+        self.fusion1 = nn.Sequential(
+            nn.Linear(d_model + pos_dim, d_model, bias=True),
+            nn.LayerNorm(d_model),
+            nn.ReLU(),
+            nn.Linear(d_model, d_model, bias=True))
+        self.fusion2 = nn.Sequential(
+            nn.Linear(d_model + pos_dim, d_model, bias=True),
+            nn.LayerNorm(d_model),
+            nn.ReLU(),
+            nn.Linear(d_model, d_model, bias=True))
 
     def forward(self, data: dict):
         valid_len = data['valid_len']
@@ -55,12 +70,26 @@ class STF_rg_hi(STF):
 
         # =============trajectory module===========================
         hist = data['hist'][:, :max_agent]
+        center = hist[...,-1,2:]
+        yaw = data['misc'][:,:max_agent,10,4]
+        pos = torch.cat([center,yaw.unsqueeze(-1)],-1)
+
+        hist[...,[0,2]]-=center[...,0].reshape(*center.shape[:2],1,1).repeat(1,1,10,2)
+        hist[..., [1, 3]] -= center[..., 1].reshape(*center.shape[:2],1,1).repeat(1,1,10,2)
+        hist[...,:2] = self._rotate(hist[...,:2],yaw)
+        hist[...,2:4] = self._rotate(hist[...,2:4],yaw)
+
         hist_mask = data['hist_mask'].unsqueeze(-2)[:, :max_agent]
         self.query_batches = self.query_embed.weight.view(1, 1, *self.query_embed.weight.shape).repeat(*hist.shape[:2],
                                                                                                        1, 1)
         hist_out = self.hist_tf(hist, self.query_batches, hist_mask, None)
 
-        # =============lane module===============================
+        pos = self.pos_emb(pos)
+        hist_out = torch.cat([pos.unsqueeze(dim=2).repeat(1, 1, self.query_batches.shape[-2], 1), hist_out], dim=-1)
+        hist_out = self.fusion1(hist_out)
+
+
+        # rg module
         lane = data['lane_vector'][:, :max_lane]
         lane_enc = self.lanenet(lane)
         lane_valid_len = data['valid_len'][:, 0]
@@ -82,6 +111,7 @@ class STF_rg_hi(STF):
         for i in range(batch_size):
             social_mask[i, 0, :social_valid_len[i]] = 1
         social_emb = self.social_emb(lane_out.view(*lane_out.shape[:2], -1))
+        social_emb = self.fusion2(torch.cat([pos, social_emb], -1))
         social_mem = self.social_enc(social_emb, social_mask)
         social_out = social_mem.unsqueeze(dim=2).repeat(1, 1, hist_out.shape[-2], 1)
         out = torch.cat([social_out, lane_out], -1)
@@ -98,3 +128,7 @@ class STF_rg_hi(STF):
         outputs_coord, outputs_class = self.prediction_head(out, new_data['obj_type'])
 
         return outputs_coord, outputs_class, new_data
+    def _rotate(self, vec, yaw):
+        c, s = torch.cos(yaw).unsqueeze(-1).repeat(1, 1, vec.shape[-2]), torch.sin(yaw).unsqueeze(-1).repeat(1, 1, vec.shape[-2])
+        vec[..., 0], vec[..., 1] = c * vec[..., 0] + s * vec[..., 1], -s * vec[..., 0] + c * vec[..., 1]
+        return vec
