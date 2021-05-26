1efddcf18316bf7711fb2df8e99790413f0c7523
Zhang Qihang <32608622+zqh0253@users.noreply.github.com>
2021-05-26 20:26:10 +0800

    Vis (#8)
    * fix huge bug
    
    * add case viser

--- utils/waymo_dataset.py
+++ utils/waymo_dataset.py
@@ -241,6 +241,7 @@ class WaymoDataset(Dataset):
         interaction_list = data['objects_of_interest'][current_valid_index]
         out['misc'] = out['misc'][current_valid_index]
         out['tracks_to_predict'] = np.pad(motion_list, [0, MAX_NBRS_NUM + 1 - valid_agent_num]).astype(bool)
+        out['misc'] = np.pad(out['misc'], [[0, MAX_NBRS_NUM + 1 - valid_agent_num], [0,0], [0,0]])
         out['objects_of_interest'] = np.pad(interaction_list, [0, MAX_NBRS_NUM + 1 - valid_agent_num]).astype(bool)
         out['misc'] = np.pad(out['misc'], [[0, MAX_NBRS_NUM + 1 - valid_agent_num], [0,0], [0,0]])
 
