80dbc19388c62a9b932c559bb6e1360cd26e18b6
zqh0253 <3170104343@zju.edu.cn>
2021-05-27 13:49:53 +0800

    tqdm

--- expand.py
+++ expand.py
@@ -1,4 +1,5 @@
 import sys, os, pickle
+from tqdm import tqdm
 
 cache_root = sys.argv[1]
 
@@ -9,7 +10,7 @@ cyc = 0
 if os.path.exists(cache_root):
     g = os.walk(cache_root)
     for path, dir_list, file_list in g:
-        for file in file_list:
+        for file in tqdm(file_list):
             with open(os.path.join(path, file), 'rb') as f:
                 data = pickle.load(f)
                 obj_type = data['obj_type'][data['tracks_to_predict']]
